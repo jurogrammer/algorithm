365c42193017d889c32fb8a3131263da305275ed
jurogrammer <solution_12@khu.ac.kr>
2022-06-07 11:25:57 +0900

    1.TwoSum

---
+++ src/main/java/leetcode/array1/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.array1;
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[0];
+    }
+}
